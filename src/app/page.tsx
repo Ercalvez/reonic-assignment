bd16c37904a8b9ebab58daf8fae6985474f42bf3
Ercalvez <erwancalv@gmail.com>
2024-08-25 07:31:29 +0200

    mock graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { ChargePointDataModel } from "@/modules/logic" 
 import SimulationInput from "@/app/simulationInput"
 import { useState } from "react";
+import SimulationGraph from "./simulationGraph";
 
 export default function Home() {
   const chargingPointDataModel = new ChargePointDataModel(20);
@@ -14,13 +15,15 @@ export default function Home() {
   const [chargingPower, setChargingPower] = useState(11);
 
   return (
-    <main className="flex min-h-screen flex-col items-left justify-between p-24">
-
+    <main className="flex min-h-screen justify-between p-24">
+      <aside className="flex flex-col space-y-1">
           <SimulationInput title="Chargepoints" inputValue = { chargePoints } setValue={setChargePoints} min={1} max={30}/>
           <SimulationInput title="Arrival Probability" inputValue = { arrivalFactor } unit = "%" setValue={setArrivalFactor} min={20} max={200}/>
           <SimulationInput title="Car Consumption" inputValue = { carConsumption } unit = "kWh / 100km" setValue={setCarConsumption} min={9} max={72}/>
           <SimulationInput title="Charging Power per Chargepoint" inputValue = { chargingPower } unit = "kW" setValue={setChargingPower} min={3} max={100}/>
           <button className="bg-red-400 h-8 px-2 my-2 rounded-md">Run simulation</button>
+      </aside>
+      <SimulationGraph />
     
     </main>
   );
